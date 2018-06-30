0fac4439ec740e45e9007c5ecdca5a8ca2545647
Tiger Sachse <tiger@sachse.io>
2018-06-29 22:58:19 -0400

    initial commit

---
+++ source/SuffixTree.java
@@ -0,0 +1,63 @@
+//TGS
+
+public class SuffixTree {
+    public String string;
+    private Node root;
+
+    public SuffixTree(String string) {
+        this.string = string;
+        buildTree();
+    }
+
+    private void buildTree() {
+        root = new Node();
+        
+        Node current = root;
+        int path = -1;
+        int length = 0;
+        int remaining = 0;
+
+        for (int i = 0; i < 3; i++) {//magic
+            remaining++;
+
+            int characterIndex = string.charAt(i) - 'a';//magic
+            while (remaining > 0) {
+
+                if (path == -1) {
+                    //at current node not down an edge
+                }
+                else {
+                    Node working = current.children[path];
+                    char match = string.charAt(working.edgeStart + length);
+
+                    
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SuffixTree tree = new SuffixTree("abc");
+    }
+}
+
+class Node {
+    public int edgeStart;
+    public int edgeStop;
+    public int index;
+    public Node[] children;
+
+    public Node() {
+        edgeStart = -1;
+        edgeStop = -1;
+        index = -1;
+        children = new Node[26];//magic
+    }
+
+    public Node(int index, int edgeStart, int edgeStop) {
+        this.edgeStart = edgeStart;
+        this.edgeStop = edgeStop;
+        this.index = index;
+        children = new Node[26];//magic
+    }
+}
