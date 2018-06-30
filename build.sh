825348bc4065511d59d059608bba496bfe8e9008
Tiger Sachse <tiger@sachse.io>
2018-06-30 13:44:48 -0400

    fancy build script

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
-javac source/SuffixTreeVisualizer/*.java
+# Build script for Suffix Tree Visualizer by Tiger Sachse
+
+# If no name is provided, resort to a default, else use the name.
+if [ -z "$1" ]
+then
+    NAME="SuffixTreeVisualizer"
+else
+    NAME=$1
+fi
+
+# Compile the source code, throw it all in a jar, and clean up.
 cd source
-jar cvfm ../dist/$1.jar SuffixTreeVisualizer/manifest.txt SuffixTreeVisualizer/* Styles/*
+javac SuffixTreeVisualizer/*.java
+jar cvfm ../dist/$NAME.jar SuffixTreeVisualizer/manifest.txt SuffixTreeVisualizer/* Styles/*
 rm SuffixTreeVisualizer/*.class
 cd ..
+
+echo ""
+echo "Suffix Tree Visualizer packaged into jar located at dist/$NAME.jar"
