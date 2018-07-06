6868bff8827bb9a2981065b4d6fb58d3b0d8f639
Tiger Sachse <tiger@sachse.io>
2018-07-05 22:46:50 -0400

    latest build and include docs in build process

--- build.sh
+++ build.sh
@@ -8,12 +8,18 @@ else
     NAME=$1
 fi
 
+# Copy the documentation.
+cp -r docs source/Documentation
+
 # Compile the source code, throw it all in a jar, and clean up.
 cd source
 javac SuffixTreeVisualizer/*.java
-jar cvfm ../dist/$NAME.jar SuffixTreeVisualizer/Manifest.txt SuffixTreeVisualizer/* Styles/*
+jar cvfm ../dist/$NAME.jar SuffixTreeVisualizer/Manifest.txt SuffixTreeVisualizer/* Styles/* Documentation/*
 rm SuffixTreeVisualizer/*.class
 cd ..
+
+# Remove the copied documentation.
+rm -r source/Documentation
 
 echo ""
 echo "Suffix Tree Visualizer packaged into jar located at dist/$NAME.jar"
